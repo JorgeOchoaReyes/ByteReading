a8ed26facfa91ae571d615163ea9b8d4c8480d08
Jorge Ochoa Reyes <jorgeochoareyes@gmail.com>
2023-01-15 22:36:06 -0800

    Added react native calendars and bottom button

--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -1,100 +1,43 @@
 import React from "react";
-
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
-import { Button } from "native-base";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { FlashList } from "@shopify/flash-list";
-import type { inferProcedureOutput } from "@trpc/server";
-import type { AppRouter } from "@acme/api";
-
-import { trpc } from "../utils/trpc";
+import { Calendar } from "react-native-calendars";
 import type { Props } from "../types/navigation";
+import Add from "../components/Add";
 
-const PostCard: React.FC<{
-  post: inferProcedureOutput<AppRouter["post"]["all"]>[number];
-}> = ({ post }) => {
+export const HomeScreen = ({}: Props) => {
   return (
-    <View className="rounded-lg border-2 border-gray-500 p-4">
-      <Text className="text-xl font-semibold text-[#cc66ff]">{post.title}</Text>
-      <Text className="text-white">{post.content}</Text>
-    </View>
-  );
-};
-
-const CreatePost: React.FC = () => {
-  const utils = trpc.useContext();
-  const { mutate } = trpc.post.create.useMutation({
-    async onSuccess() {
-      await utils.post.all.invalidate();
-    },
-  });
-
-  const [title, onChangeTitle] = React.useState("");
-  const [content, onChangeContent] = React.useState("");
-
-  return (
-    <View className="flex flex-col border-t-2 border-gray-500 p-4">
-      <TextInput
-        className="mb-2 rounded border-2 border-gray-500 p-2 text-white"
-        onChangeText={onChangeTitle}
-        placeholder="Title"
-      />
-      <TextInput
-        className="mb-2 rounded border-2 border-gray-500 p-2 text-white"
-        onChangeText={onChangeContent}
-        placeholder="Content"
-      />
-      <Button
-        onPress={() => {
-          mutate({
-            title,
-            content,
-          });
+    <SafeAreaView className="h-full w-full bg-[#00000] p-4">
+      <Text className="mx-auto pb-2 text-2xl font-bold text-black">
+        Byte Learning
+      </Text>
+      <View className="flex flex-col border-t-2 border-gray-500 p-4"></View>
+      <Calendar
+        markingType={"period"}
+        markedDates={{
+          "2023-01-15": { marked: true, dotColor: "#50cebb" },
+          "2023-01-16": { marked: true, dotColor: "#50cebb" },
+          "2023-01-21": {
+            startingDay: true,
+            color: "#50cebb",
+            textColor: "white",
+          },
+          "2023-01-22": { color: "#70d7c7", textColor: "white" },
+          "2023-01-23": {
+            color: "#70d7c7",
+            textColor: "white",
+            marked: true,
+            dotColor: "white",
+          },
+          "2023-01-24": { color: "#70d7c7", textColor: "white" },
+          "2023-01-25": {
+            endingDay: true,
+            color: "#50cebb",
+            textColor: "white",
+          },
         }}
-      >
-        Click Me
-      </Button>
-    </View>
-  );
-};
-
-export const HomeScreen = ({ navigation, route }: Props) => {
-  const postQuery = trpc.post.all.useQuery();
-  const [showPost, setShowPost] = React.useState<string | null>(null);
-
-  return (
-    <SafeAreaView className="bg-[#00000]">
-      <View className="h-full w-full p-4">
-        <Text className="mx-auto pb-2 text-5xl font-bold text-black">
-          Byte Learning
-        </Text>
-
-        <View className="py-2">
-          {showPost ? (
-            <Text className="text-white">
-              <Text className="font-semibold">Selected post:</Text>
-              {showPost}
-            </Text>
-          ) : (
-            <Text className="font-semibold italic text-black">
-              Press on a post
-            </Text>
-          )}
-        </View>
-
-        {/* <FlashList
-          data={postQuery.data}
-          estimatedItemSize={20}
-          ItemSeparatorComponent={() => <View className="h-2" />}
-          renderItem={(p) => (
-            <TouchableOpacity onPress={() => setShowPost(p.item.id)}>
-              <PostCard post={p.item} />
-            </TouchableOpacity>
-          )}
-        /> */}
-
-        <CreatePost />
-      </View>
+      />
+      <Add />
     </SafeAreaView>
   );
 };
